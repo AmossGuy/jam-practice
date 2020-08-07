c38adb2fdbec5047d77b79fdf210268c799c1abe
AmossGuy <yellowilya@gmail.com>
2020-08-07 19:37:26 -0400

    tweak handling

--- src/spaceship.rs
+++ src/spaceship.rs
@@ -7,6 +7,16 @@ use std::collections::HashSet;
 use std::f64::consts::PI;
 const TAU: f64 = 2. * PI;
 
+fn move_value_towards(value: &mut f64, goal: f64, speed: f64) {
+    if goal > *value {
+        *value += speed;
+        *value = value.min(goal);
+    } else if goal < *value {
+        *value -= speed;
+        *value = value.max(goal);
+    }
+}
+
 pub struct Spaceship {
     x: f64,
     y: f64,
@@ -30,35 +40,29 @@ impl Spaceship {
         let left = pressed_keys.contains(&KeyCode::Left);
         let right = pressed_keys.contains(&KeyCode::Right);
         let up = pressed_keys.contains(&KeyCode::Up);
+        let down = pressed_keys.contains(&KeyCode::Down);
 
-        let goal = match (left, right) {
+        let goal_a = match (left, right) {
             (true, false) => 360.,
             (false, true) => -360.,
             _ => 0.,
         };
 
-        if goal > self.ang_vel {
-            self.ang_vel += 360. * seconds;
-            self.ang_vel = self.ang_vel.min(goal);
-        } else if goal < self.ang_vel {
-            self.ang_vel -= 360. * seconds;
-            self.ang_vel = self.ang_vel.max(goal);
-        }
+        move_value_towards(&mut self.ang_vel, goal_a, 700. * seconds);
 
         self.angle += self.ang_vel * seconds;
 
-        let goal2 = match up {
+        let goal_l = match up {
             true => 100.,
             false => 0.,
         };
 
-        if goal2 > self.lin_vel {
-            self.lin_vel += 40. * seconds;
-            self.lin_vel = self.lin_vel.min(goal2);
-        } else if goal2 < self.lin_vel {
-            self.lin_vel -= 40. * seconds;
-            self.lin_vel = self.lin_vel.max(goal2);
-        }
+        let g_speed_l = match down && !up {
+            true => 160.,
+            false => 40.,
+        };
+
+        move_value_towards(&mut self.lin_vel, goal_l, g_speed_l * seconds);
 
         self.x += -(self.angle * TAU / 360.).sin() * seconds * self.lin_vel;
         self.y += (self.angle * TAU / 360.).cos() * seconds * self.lin_vel;
