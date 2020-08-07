7843ac266fdb7c42caf23db2a1f2392e179655d5
AmossGuy <yellowilya@gmail.com>
2020-08-07 17:48:51 -0400

    Commit III

--- src/main.rs
+++ src/main.rs
@@ -4,34 +4,45 @@ extern crate gate;
 gate_header!();
 
 use gate::{App, AppContext, AppInfo, KeyCode};
-use gate::renderer::{Affine, Renderer};
+use gate::renderer::Renderer;
 
 mod asset_id { include!(concat!(env!("OUT_DIR"), "/asset_id.rs")); }
-use crate::asset_id::{AssetId, SpriteId, MusicId, SoundId};
+use crate::asset_id::{AssetId, MusicId, SoundId};
+
+use std::collections::HashSet;
+use std::f64::consts::PI;
+const TAU: f64 = 2. * PI;
 
 mod spaceship;
 use spaceship::Spaceship;
 
-use std::f64::consts::PI;
-const TAU: f64 = 2. * PI;
-
 struct Game {
+    pressed_keys: HashSet<KeyCode>,
     objects: Vec<Spaceship>,
 }
 
 impl Game {
     fn new() -> Self {
         Game {
+            pressed_keys: HashSet::new(),
             objects: vec![Spaceship::new(10., 10., -45. * TAU / 360.)],
         }
     }
 }
 
 impl App<AssetId> for Game {
-    fn advance(&mut self, _seconds: f64, _ctx: &mut AppContext<AssetId>) {
+    fn advance(&mut self, seconds: f64, _ctx: &mut AppContext<AssetId>) {
+        for object in &mut self.objects {
+            object.advance(seconds, &self.pressed_keys);
+        }
     }
 
-    fn key_down(&mut self, _key: KeyCode, _ctx: &mut AppContext<AssetId>) {
+    fn key_down(&mut self, key: KeyCode, _ctx: &mut AppContext<AssetId>) {
+        self.pressed_keys.insert(key);
+    }
+
+    fn key_up(&mut self, key: KeyCode, _ctx: &mut AppContext<AssetId>) {
+        self.pressed_keys.remove(&key);
     }
 
     fn render(&mut self, renderer: &mut Renderer<AssetId>, _ctx: &AppContext<AssetId>) {
--- src/spaceship.rs
+++ src/spaceship.rs
@@ -1,6 +1,11 @@
+use gate::KeyCode;
 use gate::renderer::{Affine, Renderer};
 
 use crate::asset_id::{AssetId, SpriteId};
+
+use std::collections::HashSet;
+use std::f64::consts::PI;
+const TAU: f64 = 2. * PI;
 
 pub struct Spaceship {
     x: f64,
@@ -15,6 +20,19 @@ impl Spaceship {
             y,
             angle,
         }
+    }
+
+    pub fn advance(&mut self, seconds: f64, pressed_keys: &HashSet<KeyCode>) {
+        let left = pressed_keys.contains(&KeyCode::Left);
+        let right = pressed_keys.contains(&KeyCode::Right);
+
+        let turn = match (left, right) {
+            (true, false) => 1.,
+            (false, true) => -1.,
+            _ => 0.,
+        };
+
+        self.angle += (turn * 360. * seconds) * TAU / 360.;
     }
 
     pub fn render(&self, renderer: &mut Renderer<AssetId>) {
