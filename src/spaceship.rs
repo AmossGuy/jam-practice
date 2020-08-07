f80b50940975f429f762ba71293c16ebecc9a461
AmossGuy <yellowilya@gmail.com>
2020-08-07 18:59:23 -0400

    Commit IV

--- src/spaceship.rs
+++ src/spaceship.rs
@@ -10,7 +10,9 @@ const TAU: f64 = 2. * PI;
 pub struct Spaceship {
     x: f64,
     y: f64,
+    lin_vel: f64,
     angle: f64,
+    ang_vel: f64,
 }
 
 impl Spaceship {
@@ -18,27 +20,54 @@ impl Spaceship {
         Spaceship {
             x,
             y,
+            lin_vel: 0.,
             angle,
+            ang_vel: 0.,
         }
     }
 
     pub fn advance(&mut self, seconds: f64, pressed_keys: &HashSet<KeyCode>) {
         let left = pressed_keys.contains(&KeyCode::Left);
         let right = pressed_keys.contains(&KeyCode::Right);
+        let up = pressed_keys.contains(&KeyCode::Up);
 
-        let turn = match (left, right) {
-            (true, false) => 1.,
-            (false, true) => -1.,
+        let goal = match (left, right) {
+            (true, false) => 360.,
+            (false, true) => -360.,
             _ => 0.,
         };
 
-        self.angle += (turn * 360. * seconds) * TAU / 360.;
+        if goal > self.ang_vel {
+            self.ang_vel += 360. * seconds;
+            self.ang_vel = self.ang_vel.min(goal);
+        } else if goal < self.ang_vel {
+            self.ang_vel -= 360. * seconds;
+            self.ang_vel = self.ang_vel.max(goal);
+        }
+
+        self.angle += self.ang_vel * seconds;
+
+        let goal2 = match up {
+            true => 100.,
+            false => 0.,
+        };
+
+        if goal2 > self.lin_vel {
+            self.lin_vel += 40. * seconds;
+            self.lin_vel = self.lin_vel.min(goal2);
+        } else if goal2 < self.lin_vel {
+            self.lin_vel -= 40. * seconds;
+            self.lin_vel = self.lin_vel.max(goal2);
+        }
+
+        self.x += -(self.angle * TAU / 360.).sin() * seconds * self.lin_vel;
+        self.y += (self.angle * TAU / 360.).cos() * seconds * self.lin_vel;
     }
 
     pub fn render(&self, renderer: &mut Renderer<AssetId>) {
         let mut renderer_s = renderer.sprite_mode();
 
-        let transform = Affine::translate(self.x, self.y).pre_rotate(self.angle);
+        let transform = Affine::translate(self.x, self.y).pre_rotate(self.angle * TAU / 360.);
 
         renderer_s.draw(&transform, SpriteId::Spaceship);
     }
