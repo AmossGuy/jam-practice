3d1d5c327f865cdc13d70c6fdc02b8ff41245c8b
AmossGuy <yellowilya@gmail.com>
2020-08-08 10:46:28 -0400

    add charge effect

--- src/spaceship.rs
+++ src/spaceship.rs
@@ -7,6 +7,8 @@ use std::collections::HashSet;
 use std::f64::consts::PI;
 const TAU: f64 = 2. * PI;
 
+use rand::random;
+
 use crate::vector2::Vector2;
 
 fn move_value_towards(value: &mut f64, goal: f64, speed: f64) {
@@ -19,11 +21,16 @@ fn move_value_towards(value: &mut f64, goal: f64, speed: f64) {
     }
 }
 
+const timer_length: f64 = 1. / 16.;
+
 pub struct Spaceship {
     pos: Vector2,
     angle: f64,
     lin_vel: Vector2,
     ang_vel: f64,
+    charge: Option<f64>,
+    effect_flags: [bool; 3],
+    effect_timer: f64,
 }
 
 impl Spaceship {
@@ -33,6 +40,9 @@ impl Spaceship {
             angle,
             lin_vel: Vector2::new(0., 0.),
             ang_vel: 0.,
+            charge: None,
+            effect_flags: [false; 3],
+            effect_timer: random::<f64>() * timer_length,
         }
     }
 
@@ -41,6 +51,27 @@ impl Spaceship {
         let right = pressed_keys.contains(&KeyCode::Right);
         let up = pressed_keys.contains(&KeyCode::Up);
         let down = pressed_keys.contains(&KeyCode::Down);
+        let space = pressed_keys.contains(&KeyCode::Space);
+
+        if space && self.charge == None {
+            self.charge = Some(seconds);
+        } else if let Some(c) = &self.charge {
+            self.charge = Some(c + seconds);
+        }
+
+        if self.charge >= Some(1.) {
+            self.charge = None;
+        }
+
+        self.effect_timer += seconds;
+        if self.effect_timer >= timer_length {
+            self.effect_timer -= timer_length;
+            self.effect_flags = [
+                random::<bool>(),
+                random::<bool>(),
+                random::<bool>()
+            ];
+        }
 
         let goal_a = match (left, right) {
             (true, false) => 360.,
@@ -77,5 +108,23 @@ impl Spaceship {
                                .pre_rotate(self.angle * TAU / 360.);
 
         renderer_s.draw(&transform, SpriteId::Spaceship);
+
+        if self.charge.is_some() {
+            let mut eff_aff = Affine::translate(self.pos.x, self.pos.y);
+            let v = Vector2::from_dir_mag(self.angle, 6.);
+            eff_aff = eff_aff.post_translate(v.x, v.y); // .pre_rotate(self.angle * TAU / 360.);
+
+            if self.effect_flags[0] {
+                eff_aff = eff_aff.pre_rotate(90. * TAU / 360.);
+            }
+            if self.effect_flags[1] {
+                eff_aff = eff_aff.pre_scale_axes(-1., 1.);
+            }
+            if self.effect_flags[1] {
+                eff_aff = eff_aff.pre_scale_axes(1., -1.);
+            }
+
+            renderer_s.draw(&eff_aff, SpriteId::Charge);
+        }
     }
 }
